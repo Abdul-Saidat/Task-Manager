30aa1e80a08f58e4ac6cde871d9cf934b1ef23c4
Abdul-Saidat <yewandeabdul822@gmail.com>
2026-05-03 21:31:22 +0100

    fix: typographical error

--- src/components/ui/StatsCard.jsx
+++ src/components/ui/StatsCard.jsx
@@ -3,7 +3,7 @@ function StatsCard({ title, stats }) {
   return (
     <div>
       <p className="lg:text-xl font-medium">{title}</p>
-      <p className="text-[#7376B2] text-[14px]">{stats.total} tasks</p>
+      <p className="text-[#7376B2] text-[14px]">{stats.total} task(s)</p>
       <div className="flex justify-between">
         <p className="text-[#7376B2] text-[14px] mt-2">Progress</p>
         <p className="text-[#7376B2] text-[14px]">
